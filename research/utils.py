0904983e0e9cc08379954546a7b8a3fb8459af54
Ziqiang Feng <zf@cs.cmu.edu>
2020-09-27 23:58:17 -0400

    optimize Visualize Trajectory. Only call cv2 drawing function once

--- research/utils.py
+++ research/utils.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from operator import itemgetter
 
 from rekall.predicates import _area, _height, _width
 
@@ -10,42 +11,78 @@ from stsearch.videolib import FrameGroupInterval
 
 class VisualizeTrajectoryOnFrameGroup(Graph):
 
-    def __init__(self, trajectory_key='trajectory', parallel=1, name=None):
+    def __init__(
+        self, 
+        trajectory_key='trajectory', parallel=1, 
+        draw_line=True, draw_label=True, draw_box=False,
+        name=None):
         super().__init__()
         self.trajectory_key = trajectory_key
         self.parallel = parallel
         self.name = name
+        self.draw_line = draw_line
+        self.draw_label = draw_label
+        self.draw_box = draw_box
 
     def call(self, instream):
         tkey = self.trajectory_key
 
-        def visualize_map_fn(intrvl):
-            assert isinstance(intrvl, FrameGroupInterval)
-            assert tkey in intrvl.payload, f"{tkey} not found in {str(intrvl)}"
-            pts = np.array(list(map(centroid, intrvl.payload[tkey])))
-            assert pts.shape[1] == 2
-            # this is tricky: adjust from relative coord in original frame to pixel coord in the crop
-            pts =   (pts - [intrvl['x1'], intrvl['y1']]) / [_width(intrvl), _height(intrvl)] *  [intrvl.frames[0].shape[1], intrvl.frames[0].shape[0]]
-            pts = pts.astype(np.int32)
+        def visualize_map_fn(fg: FrameGroupInterval):
+            assert isinstance(fg, FrameGroupInterval)
+            assert tkey in fg.payload, f"{tkey} not found in {str(fg)}"
 
+            # 1. Draw all visualization on a frame with black background
+            vis_frame = np.zeros_like(fg.frames[0])
+
+            trajectory = fg.payload[tkey]
+            Xmins = list(map(itemgetter('x1'), trajectory))
+            Xmaxs = list(map(itemgetter('x2'), trajectory))
+            Ymins = list(map(itemgetter('y1'), trajectory))
+            Ymaxs = list(map(itemgetter('y2'), trajectory))
+            Xmins, Xmaxs, Ymins, Ymaxs = list(map(np.array, [Xmins, Xmaxs, Ymins, Ymaxs]))
+
+            # this is tricky: adjust from relative coord in original frame to pixel coord in the crop
+            H, W = fg.frames[0].shape[:2]
+            Xmins = W * (Xmins - fg['x1']) / _width(fg)
+            Xmaxs = W * (Xmaxs - fg['x1']) / _width(fg)
+            Ymins = H * (Ymins - fg['y1']) / _height(fg)
+            Ymaxs = H * (Ymaxs - fg['y1']) / _height(fg)
+
+            Xmins = Xmins.astype(np.int)
+            Xmaxs = Xmaxs.astype(np.int)
+            Ymins = Ymins.astype(np.int)
+            Ymaxs = Ymaxs.astype(np.int)
+
+            centroids = np.stack([0.5 * (Xmins + Xmaxs), 0.5 * (Ymins + Ymaxs)], axis=1)
+            centroids = centroids.astype(np.int)
+            assert centroids.shape == (len(trajectory), 2)
+
+            # somehow polylines doesn't work
+            # f1 = cv2.polylines(f1, pts, is_closed, color, thickness)
             color = (0, 255, 0)
             thickness = 2
             is_closed = False
 
+            for j, (p1, p2) in enumerate(zip(centroids[:-1], centroids[1:])):
+                if self.draw_line:
+                    vis_frame = cv2.line(vis_frame, tuple(p1), tuple(p2), color, thickness)
+                if self.draw_label:
+                    vis_frame = cv2.putText(vis_frame, str(j), tuple(p1), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,255,0), 1, cv2.LINE_AA)
+
+            if self.draw_box:
+                for left, top, right, bottom in zip(Xmins, Ymins, Xmaxs, Ymaxs):
+                    vis_frame = cv2.rectangle(vis_frame, (left, top), (right, bottom), (0, 0, 255), 1)
+
+            # 2. Overlay it on all frames
             new_frames = []
-
-            for fid, frame in enumerate(intrvl.frames):
+            for fid, frame in enumerate(fg.frames):
                 frame = frame.copy()
-                f1 = cv2.putText(frame, f"visualize-{fid}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
-                # somehow polylines doesn't work
-                # f1 = cv2.polylines(f1, pts, is_closed, color, thickness)
-                for j, (p1, p2) in enumerate(zip(pts[:-1], pts[1:])):
-                    f1 = cv2.line(f1, tuple(p1), tuple(p2), color, thickness)
-                    f1 = cv2.putText(f1, str(j), tuple(p1), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,0,255), 1, cv2.LINE_AA)
-                new_frames.append(f1)
+                assert vis_frame.shape == frame.shape, f"{fid} {vis_frame.shape} {frame.shape}"
+                frame[vis_frame > 0] = vis_frame[vis_frame > 0] 
+                new_frames.append(frame)
 
-            intrvl.frames = new_frames
-            return intrvl
+            fg.frames = new_frames
+            return fg
 
         return ParallelMap(
             map_fn=visualize_map_fn,
